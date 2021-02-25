d0dd6e4894a261cbdbefbea3e8467208fecf3d9c
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-02-25 16:36:01 +0100

    Fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var surname = prompt("Inserisci il tuo nome"), name = prompt("Inserisci il tuo cognnome"), favcolor = prompt("Inserisci il tuo colore preferito");
 
-var control = confirm("I dati inseriti sono corretti?\n" + surname + name + favcolor);
+var control = confirm("I dati inseriti sono corretti?\n" + "Nome: " + surname + "\nCognome: " + name + "\nColore preferito: " + favcolor);
 if (control == true) {
   document.getElementById('password').innerHTML = "La tua password è: " + surname + name + favcolor + "27";
 } else {
