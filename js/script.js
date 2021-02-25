227fe62b276b1eff5e872081649048fda3b2085f
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-02-25 17:37:10 +0100

    Ritocchi, Fix

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ var surname = prompt("Inserisci il tuo nome"), name = prompt("Inserisci il tuo c
 
 var control = confirm("I dati inseriti sono corretti?\n" + "Nome: " + surname + "\nCognome: " + name + "\nColore preferito: " + favcolor);
 if (control == true) {
-  document.getElementById('password').innerHTML = ">>>La tua password è: " + surname + name + favcolor + "27<<<";
+  document.getElementById('password').innerHTML = "La tua password è: " + surname + name + favcolor + "27";
 } else {
   alert("Ricarica la pagina");
 }
 
-document.getElementById("password").onclick = function() {randomizePassword()};
+document.getElementById("randomize_button").onclick = function() {randomizePassword()};
 function randomizePassword() {
   var digital = new Date()
   var time = digital.getMilliseconds ();
-  document.getElementById('password').innerHTML = ">>>La tua password è: " + surname + name + favcolor + time + "<<<";
+  document.getElementById('password').innerHTML = "La tua password è: " + surname + name + favcolor + time;
 }
