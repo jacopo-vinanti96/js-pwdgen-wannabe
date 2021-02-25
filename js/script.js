a0a56a1efb1a6daffa3234f0497f23711aa09d06
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-02-25 16:13:10 +0100

    Esercizio Javascript

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var surname = prompt("Inserisci il tuo nome"), name = prompt("Inserisci il tuo cognnome"), favcolor = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById('password').innerHTML = surname + name + favcolor + "27";
